31656a512a320e373165f2986df9bed6d02e7a9c
leedongyeop <ldy_1204@naver.com>
2022-10-13 19:58:47 +0900

    inventory를 만드는 메서드

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { inventory } from './inventory.model';
+import { Inventory, InventoryStatus } from './inventory.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class InventoryService {
-  private inventorys: inventory[] = [];
+  private inventorys: Inventory[] = [];
 
-  getAllInventorys(): inventory[] {
+  getAllInventorys(): Inventory[] {
     return this.inventorys;
+  }
+
+  createInventory(name: string, price: string) {
+    const inventory: Inventory = {
+      id: uuid,
+      name,
+      price,
+      status: InventoryStatus.NONSALE,
+    }
+
+    this.inventorys.push(inventory);
+    return inventory;
   }
 }
