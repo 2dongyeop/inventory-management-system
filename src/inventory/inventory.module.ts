67ed04d3e3c836f914f0baf39ce07614b8a1883a
leedongyeop <ldy_1204@naver.com>
2022-10-16 15:04:15 +0900

    inventory.repository 생성

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InventoryRepository } from './inventory.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([InventoryRepository])
+  ],
   controllers: [InventoryController],
   providers: [InventoryService]
 })
---
+++ src/inventory/inventory.repository.ts
@@ -0,0 +1,6 @@
+import { Repository } from 'typeorm';
+import { Inventory } from './inventory.entity';
+
+export class InventoryRepository extends Repository<Inventory> {
+
+}
