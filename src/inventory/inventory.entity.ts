b3622a155d187d1e28eab9f2b406cee4658ba4c7
leedongyeop <ldy_1204@naver.com>
2022-10-16 14:42:29 +0900

    typeORM을 이용한 Entity 생성

---
+++ src/inventory/inventory.entity.ts
@@ -0,0 +1,26 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { InventoryStatus } from './inventory.model';
+
+@Entity()
+export class Inventory extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: string;
+
+  @Column()
+  description?: string;
+
+  @Column()
+  manufacturer?: string;
+
+  @Column()
+  status: InventoryStatus;
+
+  @Column()
+  price_fluctuation?: string;
+}
