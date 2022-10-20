da89a73727b851017322e1f300bfb060a0633130
leedongyeop <ldy_1204@naver.com>
2022-10-20 18:42:31 +0900

    @nest/cli를 이용한 초기 코드 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { InventoryModule } from './inventory/inventory.module';
 import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { InventoryRepository } from './inventory/inventory.repository';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    InventoryModule
+    InventoryModule,
+    AuthModule
   ],
 })
 export class AppModule {}
