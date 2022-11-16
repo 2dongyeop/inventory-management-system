72aca10a5450158eb2e694a5ee9706743cc32670
leedongyeop <ldy_1204@naver.com>
2022-11-16 19:40:24 +0900

    updateUsername return fuction

--- src/user/web/dto/user-update.dto.ts
+++ src/user/web/dto/user-update.dto.ts
@@ -2,4 +2,10 @@ export class UserUpdateDto {
   username: string;
   password: string;
   updateUsername: string;
+
+  getUpdateUsername(): object {
+    return {
+      username: this.updateUsername,
+    };
+  }
 }
